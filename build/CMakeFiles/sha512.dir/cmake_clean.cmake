08e37ccd6449beac980e55eed66cb6e26e8fbe28
刘桐 <read_me@A-MacBookPro.local>
2023-11-01 21:17:13 +0800

    update

---
+++ build/CMakeFiles/sha512.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sha512.dir/src/main.cpp.o"
+  "CMakeFiles/sha512.dir/src/main.cpp.o.d"
+  "sha512"
+  "sha512.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sha512.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
